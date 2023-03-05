4fedb36c220f6b72ec3a61731421fe4d17227dc0
Traore10 <aboudramane97@gmail.com>
2023-03-05 15:22:42 +0000

    Creation du component restaurant-card

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="restaurant-card">
+        <div class="restaurant-image"></div>
+        <div class="restaurant-information">
+            <div class="top">
+                <p class="name">Subway</p>
+                <p class="note"><span>4.2</span></p>
+            </div>
+            <p class="time">30-50min</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+
+    .restaurant-card{
+        width: 32%;
+        height: 30vh;
+
+        p {
+            margin: 0;
+        }
+
+        .restaurant-image {
+            background-image: url("https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==");
+            height: 70%;
+            background-size: cover;
+            background-position: center;
+        }
+
+        .restaurant-information {
+            .top {
+                margin: 10px 0px;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+
+                .note {
+                    background-color: #eee;
+                    height: 30px;
+                    width: 30px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 0.8rem;
+                    border-radius: 50%;
+                }
+            }
+
+            .time{
+                font-size: 0.9rem;
+            }
+        }
+    }
+
+</style>
