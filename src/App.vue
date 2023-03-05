9e7974bd0c9efe45a5384ce0286679b7d3a0badc
Traore10 <aboudramane97@gmail.com>
2023-03-05 15:53:11 +0000

    Creation de restaurant row et une boocle pour dupliqué la row

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-    <RestaurantCard/>
+    <RestaurantRow/>
 </template>
 
 <script>
-import  RestaurantCard from "./components/RestaurantCard.vue";
+import  RestaurantRow from "./components/RestaurantRow.vue";
 
 export default {
   name: 'App',
   components: {
-    RestaurantCard
+    RestaurantRow
   }
 }
 </script>
@@ -18,5 +18,6 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
 #app {
   font-family: 'Roboto',Arial, sans-serif;
+  padding: 20px;
 }
 </style>
